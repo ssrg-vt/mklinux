3dbe0a24b141e81d6d3afa8e64008323333e7881
Vincent Legout <vincent@legout.info>
2015-08-27 11:18:19 -0400

    include/linux/process_server.h: Remove unused headers

--- include/linux/process_server.h
+++ include/linux/process_server.h
@@ -967,10 +967,6 @@ int process_server_try_handle_mm_fault(struct task_struct *tsk,struct mm_struct 
 		unsigned long address, unsigned long page_fault_flags,unsigned long error_code);
 void process_server_clean_page(struct page* page);
 int process_server_dup_task(struct task_struct* orig, struct task_struct* task);
-long start_distribute_operation(int operation, unsigned long addr,
-		size_t len, unsigned long prot,unsigned long new_addr,unsigned long new_len,
-		unsigned long flags,struct file *file,unsigned long pgoff);
-void end_distribute_operation(int operation,  long start_ret, unsigned long addr);
 
 long process_server_do_unmap_start(struct mm_struct *mm, unsigned long start, size_t len);
 long process_server_do_unmap_end(struct mm_struct *mm, unsigned long start, size_t len,int start_ret);
