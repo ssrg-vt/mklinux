68473ada0be4e2130059fd11ca48ed3fe299d3e8
Vincent Legout <vincent@legout.info>
2016-02-26 10:30:52 -0500

    Fix bug in commit fcc2e6c1302dbd6b55fa76b38da1d7d0cc2aeb57

--- mm/memory.c
+++ mm/memory.c
@@ -2026,17 +2026,16 @@ int fixup_user_fault(struct task_struct *tsk, struct mm_struct *mm,
         } else {
 		if (!vma || address < vma->vm_start)
 			return -EFAULT;
-	}
 
-	ret = handle_mm_fault(mm, vma, address, fault_flags);
-	if (ret & VM_FAULT_ERROR) {
-		if (ret & VM_FAULT_OOM)
-			return -ENOMEM;
-		if (ret & (VM_FAULT_HWPOISON | VM_FAULT_HWPOISON_LARGE))
-			return -EHWPOISON;
-		if (ret & VM_FAULT_SIGBUS)
-			return -EFAULT;
-		BUG();
+		ret = handle_mm_fault(mm, vma, address, fault_flags);
+		if (ret & VM_FAULT_ERROR) {
+			if (ret & VM_FAULT_OOM)
+				return -ENOMEM;
+			if (ret & (VM_FAULT_HWPOISON | VM_FAULT_HWPOISON_LARGE))
+				return -EHWPOISON;
+			if (ret & VM_FAULT_SIGBUS)
+				return -EFAULT;
+			BUG();
 		}
 	}
 	if (tsk) {
