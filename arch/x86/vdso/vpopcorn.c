0d4dceb3ea3fd85fbed30fa07e9aea2a2d0d6c67
Vincent Legout <vincent@legout.info>
2016-03-01 10:21:15 -0500

    x86/vdso: Add __vdso_popcorn_migrate

---
+++ arch/x86/vdso/vpopcorn.c
@@ -0,0 +1,18 @@
+/*
+ */
+
+#include <linux/kernel.h>
+#include <linux/getcpu.h>
+#include <linux/jiffies.h>
+#include <linux/time.h>
+#include <asm/vsyscall.h>
+#include <asm/vgtod.h>
+
+notrace long
+__vdso_popcorn_migrate(void)
+{
+	return 0;
+}
+
+long popcorn_migrate(void)
+	__attribute__((weak, alias("__vdso_popcorn_migrate")));
